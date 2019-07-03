91b8cb516422316ec1364f4d2854214e5bf5d4c4
Indiagod <pradeepa.prabhu1006@gmail.com>
2019-07-03 14:33:55 -0400

    Testcase to check for error message if incorrect password is entered

--- Desktop/Selenium /AutomationOfXero/src/test/java/XeroProject/AutomationOfXero/Xero_TestCase1.java
+++ Desktop/Selenium /AutomationOfXero/src/test/java/XeroProject/AutomationOfXero/Xero_TestCase1.java
@@ -12,7 +12,8 @@ public class Xero_TestCase1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		login();
+		//login();
+		incorrectpassword();
         
 
 	}
@@ -38,5 +39,25 @@ public class Xero_TestCase1 {
        
 	}
 	
+	public static void incorrectpassword(){
+		
+		System.setProperty("webdriver.chrome.driver", "//Users//PrabhuRamasamy//Desktop//Tekarchworks//drivers//chromedriver");
+		driver=new ChromeDriver();
+        driver.get("https://www.xero.com/us/");
+        driver.manage().window().maximize();
+        
+        WebElement Clicklogintab=driver.findElement(By.xpath("//a[@class='btn btn-tertiary-alt global-ceiling-bar-btn']"));
+        Clicklogintab.click();
+        
+        WebElement Email=driver.findElement(By.xpath("//input[@id='email']"));
+        Email.sendKeys("pradeepa.prabhu1006@gmail.com");
+        
+        WebElement Password=driver.findElement(By.xpath("//input[@id='password']"));
+        Password.sendKeys("ABCD");
+        
+        WebElement Login=driver.findElement(By.xpath("//button[@id='submitButton']"));
+        Login.click();
+		
+	}
 
 }
