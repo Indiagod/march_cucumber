5fd5c7cf5d8983127e29c1220398374fc2ce1b15
Indiagod <pradeepa.prabhu1006@gmail.com>
2019-07-03 14:27:09 -0400

    Test case for Login functionality of Xero

---
+++ Desktop/Selenium /AutomationOfXero/src/test/java/XeroProject/AutomationOfXero/Xero_TestCase1.java
@@ -0,0 +1,42 @@
+package XeroProject.AutomationOfXero;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Xero_TestCase1 {
+	
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		login();
+        
+
+	}
+	
+	public static void login(){
+		
+		System.setProperty("webdriver.chrome.driver", "//Users//PrabhuRamasamy//Desktop//Tekarchworks//drivers//chromedriver");
+		driver=new ChromeDriver();
+        driver.get("https://www.xero.com/us/");
+        driver.manage().window().maximize();
+        
+        WebElement Clicklogintab=driver.findElement(By.xpath("//a[@class='btn btn-tertiary-alt global-ceiling-bar-btn']"));
+        Clicklogintab.click();
+        
+        WebElement Email=driver.findElement(By.xpath("//input[@id='email']"));
+        Email.sendKeys("pradeepa.prabhu1006@gmail.com");
+        
+        WebElement Password=driver.findElement(By.xpath("//input[@id='password']"));
+        Password.sendKeys("Indiagod@1");
+        
+        WebElement Login=driver.findElement(By.xpath("//button[@id='submitButton']"));
+        Login.click();
+       
+	}
+	
+
+}
